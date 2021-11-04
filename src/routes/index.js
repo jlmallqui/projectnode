7b942a3e9519dc62191dc828033fdc4a6e373098
jlmallqui <jose.mallqui@gmail.com>
2021-11-04 14:43:29 -0500

    ahora e creado otro test

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 
-// router.get('/',(req, res) => {
-//     //    res.sendFile(path.join(__dirname,'views/index.html'));
-//     console.log('entro a get')
-//      res.render('index')
-//       //  console.log(path.join(__dirname,'views/index.html'))
-//     });
+ router.get('/',(req, res) => {
+     res.render('index.ejs'); 
+     });
  
 
     module.exports = router;
